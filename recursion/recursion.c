39760157768a4b041c3790685e53e731433acd52
Paris <tsellioskyp@gmail.com>
2026-07-25 17:55:10 +0300

    Week 3 of CS50x 2026 continuing, recursion, bubble, selection and merge search algorithms.

---
+++ recursion/recursion.c
@@ -0,0 +1,27 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void draw(int n);
+int main(void)
+{
+    int height = get_int("Height: ");
+    draw(height);
+}
+
+void draw(int n)
+{
+    //BASE CASE!!! OR ELSE INFINITE LOOP
+    if (n == 0) //or n<=0
+    {
+        return 1;
+    }
+    //Print a pyramid of height n-1. CAUTION!! BASE CASE!
+    draw(n-1);
+
+    //Print one more row
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}   
